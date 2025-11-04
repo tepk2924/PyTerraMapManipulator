e4e17ff854e4d43919b3ef3d1f1bbabce49954d7
tepk2924 <chohan0925@gmail.com>
2025-11-04 21:19:27 +0900

    Creating Example Script

---
+++ example_script/example.py
@@ -0,0 +1,38 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from terrariaworld import TerrariaWorld
+
+color_on_diamond_gemspark = {
+    0: "#B5B5C0",
+    1: "#C85A69",
+    2: "#C89369",
+    3: "#C8CE69",
+    4: "#8DCE69",
+    5: "#54CE69",
+    6: "#54CEA5",
+    7: "#54CEE4",
+    8: "#5493E4",
+    9: "#545AE4",
+    10: "#8D5AE4",
+    11: "#C85AE4",
+    12: "#C85AA5",
+    13: "#C8333F",
+    14: "#C8803F",
+    15: "#C8CE3F",
+    16: "#7ACE3F",
+    17: "#2DCE3F",
+    18: "#2DCE91",
+    19: "#2DCEE4",
+    20: "#2D80E4",
+    21: "#2D33E4",
+    22: "#7A33E4",
+    23: "#C833E4",
+    24: "#C83391",
+    25: "#5A6070",
+    26: "#C8CEE4",
+    27: "#979DB0",
+    28: "#C89F8F",
+    29: "#262628",
+    30: "#131816"
+}
