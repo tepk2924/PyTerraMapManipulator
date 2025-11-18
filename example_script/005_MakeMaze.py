04e9cbb00be356df7cac9899b52c24e2d7e0629d
tepk2924 <chohan0925@gmail.com>
2025-11-18 19:43:13 +0900

    .

--- example_script/005_MakeMaze.py
+++ example_script/005_MakeMaze.py
@@ -22,15 +22,15 @@ class Cell:
         self.can_D:bool = False
         self.root:Direction = -1
 
-MAZE_ROW = 60
-MAZE_COL = 100
-CELL_SIZE = 8
-G = 1 #must be gt 0, lt CELL_SIZE//2
-assert 0 < G < CELL_SIZE//2
-S = 50 #must be gt 42
-assert S > 42
+MAZE_ROW_CNT = 60
+MAZE_COL_CNT = 180
+CELL_SIZE = 30
+GATE_SIZE = 28 #must be gt 0, lt CELL_SIZE
+assert 0 < GATE_SIZE < CELL_SIZE
+MARGIN = 50 #must be gt 42
+assert MARGIN > 42
 
-maze = [[Cell() for _ in range(MAZE_COL)] for _ in range(MAZE_ROW)]
+maze = [[Cell() for _ in range(MAZE_COL_CNT)] for _ in range(MAZE_ROW_CNT)]
 
 row = 0
 col = 0
@@ -46,10 +46,10 @@ while True:
     if row > 0 and maze[row][col].can_U == False and maze[row - 1][col].is_on == False:
         dir_choices.append(Direction.U)
     #check R
-    if col < MAZE_COL - 1 and maze[row][col].can_R == False and maze[row][col + 1].is_on == False:
+    if col < MAZE_COL_CNT - 1 and maze[row][col].can_R == False and maze[row][col + 1].is_on == False:
         dir_choices.append(Direction.R)
     #check D
-    if row < MAZE_ROW - 1 and maze[row][col].can_D == False and maze[row + 1][col].is_on == False:
+    if row < MAZE_ROW_CNT - 1 and maze[row][col].can_D == False and maze[row + 1][col].is_on == False:
         dir_choices.append(Direction.D)
 
     if len(dir_choices) == 0:
@@ -65,60 +65,58 @@ while True:
             case -1:
                 break
     else:
-        try:
-            go_direction = random.choice(dir_choices)
-            match go_direction:
-                case Direction.L:
-                    maze[row][col].can_L = True
-                    col -= 1
-                    maze[row][col].can_R = True
-                    maze[row][col].root = Direction.R
-                case Direction.U:
-                    maze[row][col].can_U = True
-                    row -= 1
-                    maze[row][col].can_D = True
-                    maze[row][col].root = Direction.D
-                case Direction.R:
-                    maze[row][col].can_R = True
-                    col += 1
-                    maze[row][col].can_L = True
-                    maze[row][col].root = Direction.L
-                case Direction.D:
-                    maze[row][col].can_D = True
-                    row += 1
-                    maze[row][col].can_U = True
-                    maze[row][col].root = Direction.U
-        except Exception as e:
-            print(row, col, e)
-            exit(1)
+        go_direction = random.choice(dir_choices)
+        match go_direction:
+            case Direction.L:
+                maze[row][col].can_L = True
+                col -= 1
+                maze[row][col].can_R = True
+                maze[row][col].root = Direction.R
+            case Direction.U:
+                maze[row][col].can_U = True
+                row -= 1
+                maze[row][col].can_D = True
+                maze[row][col].root = Direction.D
+            case Direction.R:
+                maze[row][col].can_R = True
+                col += 1
+                maze[row][col].can_L = True
+                maze[row][col].root = Direction.L
+            case Direction.D:
+                maze[row][col].can_D = True
+                row += 1
+                maze[row][col].can_U = True
+                maze[row][col].root = Direction.U
 
-maze[MAZE_ROW - 1][MAZE_COL - 1].can_R = True
+maze[MAZE_ROW_CNT - 1][MAZE_COL_CNT - 1].can_R = True
 
-world = TerrariaWorld((S + CELL_SIZE*MAZE_ROW + S, S + CELL_SIZE*MAZE_COL + S))
+world = TerrariaWorld((MARGIN + CELL_SIZE*MAZE_ROW_CNT + MARGIN, MARGIN + CELL_SIZE*MAZE_COL_CNT + MARGIN))
 
-world.spawnX = S + CELL_SIZE//2
-world.spawnY = S + CELL_SIZE//2
+world.spawnX = MARGIN + CELL_SIZE//2
+world.spawnY = MARGIN + CELL_SIZE//2
 
 world.tiles.enter_editmode()
 
-for row in range(MAZE_ROW):
-    for col in range(MAZE_COL):
-        world.tiles.tileinfos[S + CELL_SIZE*row:S + CELL_SIZE*-~row, S + CELL_SIZE*col:S + CELL_SIZE*-~col, Channel.TILETYPE] = TileID.GrayBrick
-        world.tiles.tileinfos[S + 1 + CELL_SIZE*row:S - 1 + CELL_SIZE*-~row, S + 1 + CELL_SIZE*col:S - 1 + CELL_SIZE*-~col, Channel.TILETYPE] = TileID.Air
-        world.tiles.tileinfos[S + CELL_SIZE*row:S + CELL_SIZE*-~row, S + CELL_SIZE*col:S + CELL_SIZE*-~col, Channel.WALL] = WallID.GrayBrick
+for row in range(MAZE_ROW_CNT):
+    for col in range(MAZE_COL_CNT):
+        rstart = MARGIN + CELL_SIZE*row
+        rmid = rstart + CELL_SIZE//2
+        rnext = rstart + CELL_SIZE
+        cstart = MARGIN + CELL_SIZE*col
+        cmid = cstart + CELL_SIZE//2
+        cnext = cstart + CELL_SIZE
+        world.tiles.tileinfos[rstart:rnext, cstart:cnext, Channel.TILETYPE] = TileID.GrayBrick
+        world.tiles.tileinfos[rstart + 1:rnext - 1, cstart + 1:cnext - 1, Channel.TILETYPE] = TileID.Air
         if maze[row][col].can_L:
-            world.tiles.tileinfos[S + CELL_SIZE*row + G:S + CELL_SIZE*-~row - G, S + CELL_SIZE*col, Channel.TILETYPE] = TileID.Air
+            world.tiles.tileinfos[rmid - GATE_SIZE//2:rmid + GATE_SIZE//2, cstart, Channel.TILETYPE] = TileID.Air
         if maze[row][col].can_U:
-            world.tiles.tileinfos[S + CELL_SIZE*row, S + CELL_SIZE*col + G:S + CELL_SIZE*-~col - G, Channel.TILETYPE] = TileID.Air
+            world.tiles.tileinfos[rstart, cmid - GATE_SIZE//2:cmid + GATE_SIZE//2, Channel.TILETYPE] = TileID.Air
         if maze[row][col].can_R:
-            world.tiles.tileinfos[S + CELL_SIZE*row + G:S + CELL_SIZE*-~row - G, S - 1 + CELL_SIZE*-~col, Channel.TILETYPE] = TileID.Air
+            world.tiles.tileinfos[rmid - GATE_SIZE//2:rmid + GATE_SIZE//2, cnext - 1, Channel.TILETYPE] = TileID.Air
         if maze[row][col].can_D:
-            world.tiles.tileinfos[S - 1 + CELL_SIZE*-~row, S + CELL_SIZE*col + G:S + CELL_SIZE*-~col - G, Channel.TILETYPE] = TileID.Air
+            world.tiles.tileinfos[rnext - 1, cmid - GATE_SIZE//2:cmid + GATE_SIZE//2, Channel.TILETYPE] = TileID.Air
 
-world.tiles.tileinfos[S:S + CELL_SIZE*MAZE_ROW, S, Channel.WALL] = WallID.Air
-world.tiles.tileinfos[S:S + CELL_SIZE*MAZE_ROW, S + CELL_SIZE*MAZE_COL - 1, Channel.WALL] = WallID.Air
-world.tiles.tileinfos[S, S:S + CELL_SIZE*MAZE_COL, Channel.WALL] = WallID.Air
-world.tiles.tileinfos[S + CELL_SIZE*MAZE_ROW - 1, S:S + CELL_SIZE*MAZE_COL, Channel.WALL] = WallID.Air
+world.tiles.tileinfos[MARGIN + 1:MARGIN + CELL_SIZE*MAZE_ROW_CNT - 1, MARGIN + 1:MARGIN + CELL_SIZE*MAZE_COL_CNT - 1, Channel.WALL] = WallID.GrayBrick
 
 world.tiles.exit_editmode()
 
